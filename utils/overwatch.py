7da822d0fc5393f6ba15aab202b7cd5708c6e677
Jonathan Lee <dlwoaks@gmail.com>
2023-06-18 01:15:14 -0400

    update_sr function added

--- utils/overwatch.py
+++ utils/overwatch.py
@@ -1,17 +1,38 @@
 import requests
 import json
 import logging
-from bs4 import BeautifulSoup
+import apis.google as google
 
 log = logging.getLogger()
 
-def stats(battletag: str):
-    try:
-        endpoint = f"https://playoverwatch.com/en-us/career/pc/{battletag.replace('#','-')}" 
-        page = requests.get(endpoint)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        sr_elems = soup.find_all('div', class_='competitive-rank-level')
-        sr_all_roles = [sr_elem.text.strip() for sr_elem in sr_elems]
-        return (sr_all_roles[0], sr_all_roles[1:])
-    except Exception as e:
-        log.info("error occurred in overwatch stats")
+def update_sr(battletag: str):
+    client = google.authenticate_google_sheets()
+    spreadsheet = client.open_by_url(spreadsheet_url)
+    worksheet = spreadsheet.get_worksheet(0)  # Assuming you want to update the first worksheet
+
+    # Get all rows of data (excluding the header row)
+    data = worksheet.get_all_values()[1:]
+
+    for index, row in enumerate(data):
+        # Assuming the columns are as follows: A - BattleTag, B - Tank SR, C - DPS SR, D - Support SR
+        battletag = row[0]
+
+        # API Reference: https://overfast-api.tekrop.fr/#tag/Players/operation/get_player_summary_players__player_id__summary_get
+        endpoint = f"https://overfast-api.tekrop.fr/players/{battletag.replace('#','-')}/summary"
+        response = requests.get(endpoint)
+        if response.status_code == 200:
+            player_data = response.json()
+            tank_rank = player_data['competitive']['season']['tank']['division'] + " " + player_data['competitive']['season']['tank']['tier']
+            damage_rank = player_data['competitive']['season']['damage']['division'] + " " + player_data['competitive']['season']['damage']['tier']
+            support_rank = player_data['competitive']['season']['support']['division'] + " " + player_data['competitive']['season']['support']['tier']
+
+        else:
+            log.info(f'failed to update_sr for {battletag}. validate btag format and/or casing') 
+
+    # Update the corresponding cells with the SR values
+    row_index = account_index + 2  # Assuming the first row is for headers
+    worksheet.update_cell(row_index, 2, tank_rank)  # Update tank SR in column B
+    worksheet.update_cell(row_index, 3, damage_rank)  # Update DPS SR in column C
+    worksheet.update_cell(row_index, 4, support_rank)  # Update support SR in column D
+
+    log.info('update_sr completed')
